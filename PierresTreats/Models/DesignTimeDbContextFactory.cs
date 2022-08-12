9ba2ee3b047ade940ca8b09956daa973093e70db
FrankTimmons <franktimmonspdx@gmail.com>
2022-08-12 11:11:32 -0700

    Fix small formatting on some model files

--- PierresTreats/Models/DesignTimeDbContextFactory.cs
+++ PierresTreats/Models/DesignTimeDbContextFactory.cs
@@ -7,13 +7,12 @@ namespace PierresTreats.Models
 {
   public class PierresTreatsContextFactory : IDesignTimeDbContextFactory<PierresTreatsContext>
   {
-
     PierresTreatsContext IDesignTimeDbContextFactory<PierresTreatsContext>.CreateDbContext(string[] args)
     {
       IConfigurationRoot configuration = new ConfigurationBuilder()
-          .SetBasePath(Directory.GetCurrentDirectory())
-          .AddJsonFile("appsettings.json")
-          .Build();
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .Build();
 
       var builder = new DbContextOptionsBuilder<PierresTreatsContext>();
 
