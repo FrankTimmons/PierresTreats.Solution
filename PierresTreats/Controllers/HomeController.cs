b456d334bf4d5bf7c610a9154b66f9483c347a3f
FrankTimmons <franktimmonspdx@gmail.com>
2022-08-15 08:43:25 -0700

    Add viewbags to controller.

--- PierresTreats/Controllers/HomeController.cs
+++ PierresTreats/Controllers/HomeController.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using PierresTreats.Models;
+using System.Linq;
 
 namespace PierresTreats.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly PierresTreatsContext _db;
+
+    public HomeController(PierresTreatsContext db)
+    {
+      _db = db;
+    }
     [HttpGet("/")]
     public ActionResult Index()
     {
+      ViewBag.TreatList = _db.Treats.ToList();
+      ViewBag.FlavorList = _db.Flavors.ToList();
       return View();
     }
   }
